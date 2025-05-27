35fa69907961a970e5879c0e84b61cd58d582438
Farouk Touil <farouktouil@hotmail.com>
2025-05-27 13:34:43 +0200

    initial

---
+++ script.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        // sky: colors.sky,
+        // cyan: colors.cyan
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
